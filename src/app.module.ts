c816ba0bf8af939d2ae51533bafa90f4ef39060d
Cao Duc <caohongduc0910@gmail.com>
2024-11-22 15:28:51 +0700

    feat: build base project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
